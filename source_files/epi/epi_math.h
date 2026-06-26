7b9600dfa6ccfac10b399dda4653291fed275a27
scramshackle <scramshackle@proton.me>
2026-06-25 18:28:17 -0600

    Internalize PRNS logic

--- source_files/epi/epi_math.h
+++ source_files/epi/epi_math.h
@@ -88,6 +88,49 @@ inline bool AlmostEquals(double first, double second)
     return distance <= kAlmostEqualsMaxUlps;
 }
 
+class RNG
+{
+public:
+    void     Init(uint64_t seed)   { i_ = kWeyl * seed; }
+    void     Set(uint64_t pos)     { i_ = kWeyl * pos; }
+    void     Seek(int64_t offset)  { i_ += kWeyl * (uint64_t)offset; }
+    uint64_t Tell() const          { return i_ * kWeylI; }
+    uint64_t Peek() const          { return Mix(i_); }
+
+    static uint64_t At(uint64_t n) { return Mix(kWeyl * n); }
+
+    uint64_t Next()
+    {
+        uint64_t i = i_;
+        uint64_t r = Mix(i);
+        i_         = i + kWeyl;
+        return r;
+    }
+    uint64_t Prev()
+    {
+        uint64_t i = i_;
+        uint64_t r = Mix(i);
+        i_         = i - kWeyl;
+        return r;
+    }
+
+private:
+    static uint64_t Mix(uint64_t x)
+    {
+        x ^= (x >> 31);
+        x *= UINT64_C(0x7fb5d329728ea185);
+        x ^= (x >> 27);
+        x *= UINT64_C(0x81dadef4bc2dd44d);
+        x ^= (x >> 33);
+        return x;
+    }
+
+    static constexpr uint64_t kWeyl  = UINT64_C(0x61c8864680b583eb);
+    static constexpr uint64_t kWeylI = UINT64_C(0x0e217c1e66c88cc3);
+
+    uint64_t i_ = 0;
+};
+
 } // namespace epi
 
 //--- editor settings ---
